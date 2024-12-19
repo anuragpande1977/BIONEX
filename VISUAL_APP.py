b72bf418224ca58446869077616d8b3e2638ac90
anuragpande1977 <137955688+anuragpande1977@users.noreply.github.com>
2024-12-19 10:53:32 -0500

    Update VISUAL_APP.py

--- VISUAL_APP.py
+++ VISUAL_APP.py
@@ -10,29 +10,6 @@ import gdown
 import zipfile
 import streamlit as st
 import streamlit as st
-import streamlit.components.v1 as components
-
-# Function to inject Google Analytics
-def inject_google_analytics():
-    GA_TRACKING_ID = "G-JR76W8BFHL"  # Replace with your Google Analytics Tracking ID
-    analytics_script = f"""
-    <script async src="https://www.googletagmanager.com/gtag/js?id={GA_TRACKING_ID}"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){{dataLayer.push(arguments);}}
-      gtag('js', new Date());
-      gtag('config', '{GA_TRACKING_ID}');
-    </script>
-    """
-    # Use Streamlit components to inject JavaScript without affecting app flow
-    components.html(analytics_script, height=0, width=0)
-
-# Inject Google Analytics script
-inject_google_analytics()
-
-# Main app content
-st.title("BIONEXT FOR BIOMEDICAL DATAMINING")
-st.write("Welcome to BIONEXT, your go-to platform for biomedical data mining!")
 
 
 
